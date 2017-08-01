4a074a74d315d4fd29c87a432a69a5e636a4e858
Jason Ogato <jason.ogato@bison.howard.edu>
2017-07-31 17:55:03 -0700

    Object model for user

--- app/src/main/java/com/jogato/climate/User.java
+++ app/src/main/java/com/jogato/climate/User.java
@@ -1,8 +1,52 @@
 package com.jogato.climate;
 
-/**
- * Created by jogato on 7/31/17.
- */
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class User {
+    private String mUserName;
+    private String mUserEmail;
+    private String mUserID;
+    private static FirebaseAuth mUserAuthState;
+    private static User sUser;
+
+    public static User getInstance(){
+        if(sUser == null){
+            sUser = new User();
+        }
+        return sUser;
+    }
+
+    public String getmUserName() {
+        return mUserName;
+    }
+
+    public void setmUserName(String mUserName) {
+        this.mUserName = mUserName;
+    }
+
+    public String getmUserEmail() {
+        return mUserEmail;
+    }
+
+    public void setmUserEmail(String mUserEmail) {
+        this.mUserEmail = mUserEmail;
+    }
+
+    public String getmUserID() {
+        return mUserID;
+    }
+
+    public void setmUserID(String mUserID) {
+        this.mUserID = mUserID;
+    }
+
+    public static FirebaseAuth getmUserAuthState() {
+        return mUserAuthState;
+    }
+
+    public void setmUserAuthState(FirebaseAuth auth) {
+        mUserAuthState = auth;
+    }
+
 }
