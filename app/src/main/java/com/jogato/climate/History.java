9f654d504964d3be4a65eb4bb83fae6b640f3e46
Jason Ogato <jason.ogato@bison.howard.edu>
2017-08-10 16:39:46 -0700

    Object model for history entries

--- app/src/main/java/com/jogato/climate/History.java
+++ app/src/main/java/com/jogato/climate/History.java
@@ -1,13 +1,21 @@
 package com.jogato.climate;
 
 
+import com.google.firebase.database.ServerValue;
+
+import java.util.Map;
+
+import static com.google.firebase.database.ServerValue.TIMESTAMP;
+
 public class History {
     private String mWeatherURL;
     private String mZipCode;
+    private Map<String, String> mTimeStamp;
 
-    public History(String weatherURL, String zipCode){
+    public History(String weatherURL, String zipCode, Map<String, String>timeStamp){
         mWeatherURL = weatherURL;
         mZipCode = zipCode;
+        mTimeStamp = timeStamp;
     }
 
     public String getmWeatherURL() {
