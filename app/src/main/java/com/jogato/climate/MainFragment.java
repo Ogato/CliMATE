af0c5584ee09c4c6fe1e6183708449a68976a648
Jason Ogato <jason.ogato@bison.howard.edu>
2017-08-09 23:45:42 -0700

    .

--- app/src/main/java/com/jogato/climate/MainFragment.java
+++ app/src/main/java/com/jogato/climate/MainFragment.java
@@ -4,11 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.transition.ArcMotion;
-import android.transition.Explode;
 import android.transition.Fade;
-import android.transition.Slide;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +15,6 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        Log.i("JO_INFO", "ONCREATE");
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setEnterTransition(new Fade());
@@ -27,10 +22,10 @@ public class MainFragment extends Fragment {
         }
     }
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.i("JO_INFO", "CREATEVIEW");
         View v = inflater.inflate(R.layout.fragment_main, container, false);
         if(getActivity().getActionBar() != null){
             getActivity().getActionBar().show();
