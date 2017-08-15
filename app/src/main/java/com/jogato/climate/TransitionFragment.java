89eaf468f7c9ef6ab38d456a299306fc2e8a8580
Harold Lomotey <hlomotey@hlomotey-macbookpro2.roam.corp.google.com>
2017-08-15 14:20:31 -0700

    Three transition images

--- app/src/main/java/com/jogato/climate/TransitionFragment.java
+++ app/src/main/java/com/jogato/climate/TransitionFragment.java
@@ -1,20 +1,20 @@
 package com.jogato.climate;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by jogato on 8/7/17.
@@ -23,6 +23,8 @@ import android.widget.TextView;
 public class TransitionFragment extends Fragment {
     private String mCaption;
     private TextView loadingCaption;
+
+    ImageView gifImage;
 
 
     @Override
@@ -53,15 +55,45 @@ public class TransitionFragment extends Fragment {
         return v;
     }
 
-    /*@Override
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mLinearLayout = (LinearLayout) view.findViewById(R.id.transition);
+        /*mLinearLayout = (LinearLayout) view.findViewById(R.id.transition);
         ColorDrawable[] color = {new ColorDrawable(Color.RED), new ColorDrawable(Color.BLUE)};
         TransitionDrawable trans = new TransitionDrawable(color);
         mLinearLayout.setBackground(trans);
-        trans.startTransition(6000);
+        trans.startTransition(6000);*/
+
+
+        gifImage = (ImageView) view.findViewById(R.id.gifImageView);
+
+
+        Random rand = new Random();
+        int gifcount = rand.nextInt(4);
+
+
+        if (gifcount == 0) {
+            gifImage.setImageResource(R.drawable.gifimagethree);
+            gifImage.setBackgroundColor(Color.parseColor("#000000"));
+            view.setBackgroundColor(Color.parseColor("#000000"));
+
+        } else if (gifcount == 1){
+
+            gifImage.setImageResource(R.drawable.gifimagetwo);
+            gifImage.setBackgroundColor(Color.parseColor("#80D3F8"));
+            view.setBackgroundColor(Color.parseColor("#80D3F8"));
+
+
+
+        } else {
+            gifImage.setImageResource(R.drawable.gifimageone);
+            gifImage.setBackgroundColor(Color.parseColor("#69BDEE"));
+            view.setBackgroundColor(Color.parseColor("#69BDEE"));
+        }
+
+
+
     }
-    */
+
 
 }
